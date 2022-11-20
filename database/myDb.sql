739f9c54bffccf70fa892628d04111f883b30871
JPSchmitz <jpschmitz@web.de>
2022-11-21 02:34:43 +0800

    ja

---
+++ database/myDb.sql
@@ -0,0 +1,17 @@
+SET time_zone = "+08:00";
+
+
+CREATE TABLE `Person` (
+  `id` int NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `age` int
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE USER 'exporter'@'localhost' IDENTIFIED BY 'exporter';
+GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'exporter'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'exporter'@'localhost';
+
+INSERT INTO `Person` (`id`, `name`,`age`) VALUES
+(1, 'William',40),
+(2, 'Marc',17),
+(3, 'John',60);
